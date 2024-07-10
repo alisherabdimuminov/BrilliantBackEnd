58452d99d5558d4a2b1ebb057b2b649be10ea4a6
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-07-10 15:15:53 +0500

    changing date format

--- counting/serializers.py
+++ counting/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from .models import Model
 
 class ModelSerializer(serializers.ModelSerializer):
+    created = serializers.DateTimeField(format="%m-%d %H:%M:%S")
     class Meta:
         model = Model
         fields = ("camera_id", "created", "number_of_people", "image", "type", )
